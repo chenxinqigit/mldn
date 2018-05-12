b60ca7c3a0c6b9e3b0ee6d69f86656b24b6e4484
xiaoqiqi <694313848@qq.com>
2018-05-12 11:10:11 +0800

    Change Two File

--- Demo.java
+++ Demo.java
@@ -1,6 +1,7 @@
 public class Demo{
 	public static void main(String args[]){
-		System.out.println("Demo world");
-		System.out.println("Demo Girl friend what is?");
+		/*System.out.println("Demo world");
+		System.out.println("Demo Girl friend what is?");*/
+		System.out.println("Current Time="+new java.util.Date());
 	}
 }
--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 public class Hello{
 	public static void main(String args[]){
-		System.out.println("Hello world");
-		System.out.println("Girl friend what is?");
+		/*System.out.println("Hello world");
+		System.out.println("Girl friend what is?");*/
+		System.out.println("你好世界Q!");
 	}
 }
