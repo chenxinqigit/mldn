cb3b6987ec297da97b2bd10fc88bb0b723853203
xiaoqiqi <694313848@qq.com>
2018-05-13 10:25:20 +0800

    Change Tow Files Print Content

--- Demo.java
+++ Demo.java
@@ -2,6 +2,6 @@ public class Demo{
 	public static void main(String args[]){
 		/*System.out.println("Demo world");
 		System.out.println("Demo Girl friend what is?");*/
-		System.out.println("Current Time="+new java.util.Date());
+		System.out.println("Time is Money");
 	}
 }
--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ public class Hello{
 	public static void main(String args[]){
 		/*System.out.println("Hello world");
 		System.out.println("Girl friend what is?");*/
-		System.out.println("你好世界Q!");
+		System.out.println("Hello World!");
 	}
 }
